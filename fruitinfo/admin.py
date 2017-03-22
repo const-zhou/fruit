b916c0d6904f1747c0f2cfc893fb144e2f8030de
周敦广 <const_zhou@zhoudunguangdeMacBook-Pro.local>
2017-03-22 23:24:08 +0800

    add fruit model

--- fruitinfo/admin.py
+++ fruitinfo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import SectionItem
+from .models import Fruit
+
+admin.site.register(SectionItem)
+admin.site.register(Fruit)
--- fruitinfo/models.py
+++ fruitinfo/models.py
@@ -3,3 +3,15 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Fruit(models.Model):
+    name = models.CharField(max_length = 512)
+    description = models.TextField()
+
+class SectionItem(models.Model):
+    section = models.CharField(max_length = 512)
+    content = models.TextField()
+    itemType = models.IntegerField()
+    sectionItem = models.ForeignKey(Fruit, related_name='fruit_section') 
+
+
